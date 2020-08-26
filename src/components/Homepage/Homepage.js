75f158b8a5023a39e58bda5da82718585b067440
emsimajki <66696140+emsimajki@users.noreply.github.com>
2020-08-27 01:42:38 +0200

    Added factors and names

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -3,6 +3,17 @@ import "./_homepage.scss";
 import React from "react";
 
 export const Homepage = (props) => {
+  const data = [];
+
+  const handleForm = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+    for (let i = 0; i < 36; i++) {
+      data.push(+e.target[`st${i}`].value);
+    }
+    console.log(data);
+  };
+
   return (
     <>
       <section className="homepage">
@@ -11,155 +22,155 @@ export const Homepage = (props) => {
         </article>
         <article className="homepage__survey">
           <h2>Ankieta</h2>
-          <form className="homepage__form">
+          <form className="homepage__form" onSubmit={handleForm}>
             <div className="homepage__technical">
               <label>
                 Długie wrzuty:
-                <input type="number" min={0} max={10} />
+                <input type="number" name="st0" min={0} max={10} />
               </label>
               <label>
                 Dośrodkowanie:
-                <input type="number" min={0} max={10} />
+                <input type="number" name="st1" min={0} max={10} />
               </label>
               <label>
                 Drybling:
-                <input type="number" min={0} max={10} />
+                <input type="number" name="st2" min={0} max={10} />
               </label>
               <label>
                 Gra głową:
-                <input type="number" min={0} max={10} />
+                <input type="number" name="st3" min={0} max={10} />
               </label>
               <label>
                 Krycie:
-                <input type="number" min={0} max={10} />
+                <input type="number" name="st4" min={0} max={10} />
               </label>
               <label>
                 Odbiór piłki:
-                <input type="number" min={0} max={10} />
+                <input type="number" name="st5" min={0} max={10} />
               </label>
               <label>
                 Podanie:
-                <input type="number" min={0} max={10} />
+                <input type="number" name="st6" min={0} max={10} />
               </label>
               <label>
                 Przyjęcie piłki:
-                <input type="number" min={0} max={10} />
+                <input type="number" name="st7" min={0} max={10} />
               </label>
               <label>
                 Rzuty karne:
-                <input type="number" min={0} max={10} />
+                <input type="number" name="st8" min={0} max={10} />
               </label>
               <label>
                 Rzuty rożne:
-                <input type="number" min={0} max={10} />
+                <input type="number" name="st9" min={0} max={10} />
               </label>
               <label>
                 Rzuty wolne:
-                <input type="number" min={0} max={10} />
+                <input type="number" name="st10" min={0} max={10} />
               </label>
               <label>
                 Strzały z dystansu:
-                <input type="number" min={0} max={10} />
+                <input type="number" name="st11" min={0} max={10} />
               </label>
               <label>
                 Technika:
-                <input type="number" min={0} max={10} />
+                <input type="number" name="st12" min={0} max={10} />
               </label>
               <label>
                 Wykańczanie akcji:
-                <input type="number" min={0} max={10} />
+                <input type="number" name="st13" min={0} max={10} />
               </label>
             </div>
             <div className="homepage__mental">
               <label>
                 Agresja:
-                <input type="number" min={0} max={10} />
+                <input type="number" name="st14" min={0} max={10} />
               </label>
               <label>
                 Błyskotliwość:
-                <input type="number" min={0} max={10} />
+                <input type="number" name="st15" min={0} max={10} />
               </label>
               <label>
                 Decyzje:
-                <input type="number" min={0} max={10} />
+                <input type="number" name="st16" min={0} max={10} />
               </label>
               <label>
                 Determinacja:
-                <input type="number" min={0} max={10} />
+                <input type="number" name="st17" min={0} max={10} />
               </label>
               <label>
                 Gra bez piłki:
-                <input type="number" min={0} max={10} />
+                <input type="number" name="st18" min={0} max={10} />
               </label>
               <label>
                 Koncentracja:
-                <input type="number" min={0} max={10} />
+                <input type="number" name="st19" min={0} max={10} />
               </label>
               <label>
                 Opanowanie:
-                <input type="number" min={0} max={10} />
+                <input type="number" name="st20" min={0} max={10} />
               </label>
               <label>
                 Prcowitość:
-                <input type="number" min={0} max={10} />
+                <input type="number" name="st21" min={0} max={10} />
               </label>
               <label>
                 Przegląd sytuacji:
-                <input type="number" min={0} max={10} />
+                <input type="number" name="st22" min={0} max={10} />
               </label>
               <label>
                 Przewidywanie:
-                <input type="number" min={0} max={10} />
+                <input type="number" name="st23" min={0} max={10} />
               </label>
               <label>
                 Przywództwo:
-                <input type="number" min={0} max={10} />
+                <input type="number" name="st24" min={0} max={10} />
               </label>
               <label>
                 Ustawianie się:
-                <input type="number" min={0} max={10} />
+                <input type="number" name="st25" min={0} max={10} />
               </label>
               <label>
                 Waleczność:
-                <input type="number" min={0} max={10} />
+                <input type="number" name="st26" min={0} max={10} />
               </label>
               <label>
                 Współpraca:
-                <input type="number" min={0} max={10} />
+                <input type="number" name="st27" min={0} max={10} />
               </label>
             </div>
             <div className="homepage__physical">
               <label>
                 Przyspieszenie:
-                <input type="number" min={0} max={10} />
+                <input type="number" name="st28" min={0} max={10} />
               </label>
               <label>
                 Równowaga:
-                <input type="number" min={0} max={10} />
+                <input type="number" name="st29" min={0} max={10} />
               </label>
               <label>
                 Siła:
-                <input type="number" min={0} max={10} />
+                <input type="number" name="st30" min={0} max={10} />
               </label>
               <label>
                 Skoczność:
-                <input type="number" min={0} max={10} />
+                <input type="number" name="st31" min={0} max={10} />
               </label>
               <label>
                 Sprawność:
-                <input type="number" min={0} max={10} />
+                <input type="number" name="st32" min={0} max={10} />
               </label>
               <label>
                 Szybkość:
-                <input type="number" min={0} max={10} />
+                <input type="number" name="st33" min={0} max={10} />
               </label>
               <label>
                 Wytrzymałość:
-                <input type="number" min={0} max={10} />
+                <input type="number" name="st34" min={0} max={10} />
               </label>
               <label>
                 Zwinność:
-                <input type="number" min={0} max={10} />
+                <input type="number" name="st35" min={0} max={10} />
               </label>
               <input
                 className="homepage__submit"
